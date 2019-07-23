ab9e658738071eddc2f03000dbe0731afd4fba5b
Jennifer Birsan <jennibirsan@Cornels-MBP.attlocal.net>
2019-07-23 15:05:08 -0500

    routes complete

---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+// ===============================================================================
+// CODE COPIED AND TWEAKED FROM HOT RESTAURANT CLASS ACTIVITY 15 IN WEEK 13
+
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/survey", function(req, res) {
+    res.json(friendsData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/survey", function(req, res) {
+      friendsData.push(req.body);
+      res.json(true);
+  });
+
+};
